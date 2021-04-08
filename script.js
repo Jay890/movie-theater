ca3806af7ba14884ada0ef20fa993c5b01198894
Jason Leong <vergil_dmc1@hotmail.com>
2021-04-08 10:41:43 +1000

    Storing local data
    using localStorage built in browser feature to store some data

--- script.js
+++ script.js
@@ -13,8 +13,38 @@ let ticketPrice = +movieSelect.value;
 // const ticketNumber = +count;
 // const totalPrice = +total;
 
+// Save the selected movie index and price
+function setMovieData(movieIndex, moviePrice) {
+  localStorage.setItem('selectedMovieIndex', movieIndex);
+  localStorage.setItem('selectedMoviePrice', moviePrice);
+}
+
 function updateSelectedCount() {
+  // nodelist (like an array)
   const selectSeats = document.querySelectorAll('.row .seat.selected');
+
+  /* We need to basically create an array of indexes to save the seats as 
+the nodelist does not tell much 
+3 steps to do this: 
+- Copy selected seats into an array
+- Map through array
+- return a new array indexes
+*/
+
+  /* spread operator does not copy the whole array [a,b,c] 
+  but passes (add in) through the values a,b,c to the new array we are making
+  Overall convert the node list to an array */
+
+  // map returns an array
+  const seatsIndex = [...selectSeats].map((seat) => {
+    return [...seats].indexOf(seat);
+  });
+
+  // store the values into a local storage save strings (built-in browser)
+  // store the indexes, key value pair, wrap into a string with JSON as seatsIndex is array
+  localStorage.setItem('selectedSeats', JSON.stringify(seatsIndex));
+
+  console.log(seatsIndex);
   const selectSeatsCount = selectSeats.length;
 
   count.innerText = selectSeatsCount;
@@ -24,6 +54,7 @@ function updateSelectedCount() {
 // Movie Select Event
 movieSelect.addEventListener('change', (e) => {
   ticketPrice = e.target.value;
+  setMovieData(e.target.selectedIndex, e.target.value);
   updateSelectedCount();
 });
 
